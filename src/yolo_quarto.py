80e70a211c9aab1fc4a69bde2af46541bee37eab
清岡優祐 <kiyookayusuke@kiyooka.local>
2021-12-18 13:35:08 +0900

    Board threshold setting

--- src/yolo_quarto.py
+++ src/yolo_quarto.py
@@ -23,10 +23,10 @@ class ObjectTracker():
         rospy.init_node('object_tracking', anonymous=True)
         self._cv_bridge = CvBridge()
         self._point_of_centroid = None
-        self.object_place = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        self.final_object_place = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        self.object_name = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        self.board_place = [(x_min,x_max,y_min,y_max),(),()......] 
+        self.object = np.zeros(16, dtype=int)
+        self.final_object_place = np.zeros(17,dtype=int)
+        self.object_place = np.zeros(17,dtype=int)
+        self.board_place = [(115,136,33,63),(150,162,39,69),(178,195,46,76),(212,222,40,84),(108,125,51,81),(140,157,59,92),(173,187,57,95),(211,225,64,102),(93,115,66,107),(128,147,83,116),(165,182,81,116),(202,217,91,132),(81,103,103,137),(116,136,111,144),(156,177,109,155),(195,209,113,162,(110,137,174,216))] #ボードの閾値 
 	self.yolo_pub = rospy.Publisher("yolo_result", String, queue_size=10)
 
 
@@ -53,64 +53,64 @@ class ObjectTracker():
                 point = (x_center, y_center)
                 print(point)
                 if box.Class == "b_l_s_0": # black_long_square_no hole
-                    self.object_place[0] = point
-                    self.object_name[0] = box.class
+                    self.object[0] = point
+                    self.object_place[0] = box.class
                 if box.Class == "b_l_s_1":  
-                    self.object_place[1] = point
-                    self.object_name[1] = box.class
+                    self.object[1] = point
+                    self.object_place[1] = box.class
                 if box.Class == "b_l_c_0":  
-                    self.object_place[2] = point
-                    self.object_name[2] = box.class
+                    self.object[2] = point
+                    self.object_place[2] = box.class
                 if box.Class == "b_l_c_1":  
-                    self.object_place[3] = point
-                    self.object_name[3] = box.class
+                    self.object[3] = point
+                    self.object_place[3] = box.class
                 if box.Class == "b_s_s_0":  
-                    self.object_place[4] = point
-                    self.object_name[4] = box.class
+                    self.object[4] = point
+                    self.object_place[4] = box.class
                 if box.Class == "b_s_s_1":
-                    self.object_place[5] = point
-                    self.object_name[5] = box.class
+                    self.object[5] = point
+                    self.object_place[5] = box.class
                 if box.Class == "b_s_s_0":  
-                    self.object_place[6] = point
-                    self.object_name[6] = box.class
+                    self.object[6] = point
+                    self.object_place[6] = box.class
                 if box.Class == "b_s_c_1":  
-                    self.object_place[7] = point
-                    self.object_name[7] = box.class
+                    self.object[7] = point
+                    self.object_place[7] = box.class
                 if box.Class == "w_l_s_0":  
-                    self.object_place[8] = point
-                    self.object_name[8] = box.class
+                    self.object[8] = point
+                    self.object_place[8] = box.class
                 if box.Class == "w_l_s_1":  
-                    self.object_place[9] = point
-                    self.object_name[9] = box.class
+                    self.object[9] = point
+                    self.object_place[9] = box.class
                 if box.Class == "w_l_c_0":  
-                    self.object_place[10] = point
-                    self.object_name[10] = box.class
+                    self.object[10] = point
+                    self.object_place[10] = box.class
                 if box.Class == "w_l_c_1":  
-                    self.object_place[11] = point
-                    self.object_name[11] = box.class
+                    self.object[11] = point
+                    self.object_place[11] = box.class
                 if box.Class == "w_s_s_0":  
-                    self.object_place[12] = point
-                    self.object_name[12] = box.class
+                    self.object[12] = point
+                    self.object_place[12] = box.class
                 if box.Class == "w_s_s_1":  
-                    self.object_place[13] = point
-                    self.object_name[13] = box.class
+                    self.object[13] = point
+                    self.object_place[13] = box.class
                 if box.Class == "w_s_s_0":  
-                    self.object_place[14] = point
-                    self.object_name[14] = box.class
+                    self.object[14] = point
+                    self.object_place[14] = box.class
                 if box.Class == "w_s_c_1": # white_short_circle_hole 
-                    self.object_place[15] = point
-                    self.object_name[15] = box.class
+                    self.object[15] = point
+                    self.object_place[15] = box.class
                 else:  # false (recognized white_line in boundingboxes)
                     point = False
         else:  # false (recognized objects inboundingboxes)
             point = False
-        return self.object_place
+        return self.object
 
     def quarto_recognition(self):
-        for i in range(16): #object
+        for i in range(16): 
             for j in range(17):
-                if self.board_place[j][0] < self.object_place[i][0] < self.board_place[j][1] and self.board_place[j][2] < self.object_place[i][1] < self.board_place[j][3]:
-                    self.final_object_place[j] = self.object_name[i]
+                if self.board_place[j][0] < self.object[i][0] < self.board_place[j][1] and self.board_place[j][2] < self.object[i][1] < self.board_place[j][3]:
+                    self.final_object_place[j] = self.object_place[i]
 
         self.yolo_pub.publish(self.final_object_place)
 
